ad222bd60d6d80f8d3d09c3c6e6f8f7f0343d6f5
Jairaj01012002 <54246473+Jairaj01012002@users.noreply.github.com>
2022-05-05 16:18:01 +0530

    Add files via upload

---
+++ CEN 300 project code/tracker/object_tracking.py
@@ -0,0 +1,7 @@
+class object_tracking:
+	def __init__(self, objectID, centroid):
+		# store the object ID, then initialize a list of centroids
+		# using the current centroid
+		self.objectID = objectID
+		self.centroids = [centroid]
+		self.counted = False
